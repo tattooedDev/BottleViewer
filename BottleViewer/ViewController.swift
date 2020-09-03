532bec9b23ff5c069824f38d685563af8cf5adaf
Dennis Parussini <thedan84@mac.com>
2020-09-03 08:16:10 +0200

    Initial Commit

---
+++ BottleViewer/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  BottleViewer
+//
+//  Created by Dennis Parussini on 03.09.20.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
